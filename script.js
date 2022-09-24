b4e4746e039f32aec09c43a7272e9d86c50d3bce
Abit Salihu <abiti.salihu@gmail.com>
2022-09-24 13:05:12 +0200

    add dark mode logic

---
+++ script.js
@@ -0,0 +1,38 @@
+//! dark mode
+
+const header = document.querySelector(".header");
+const toggle = document.querySelector(".toggle-mode");
+const body = document.querySelector("body");
+const paragraph = document.querySelector("p");
+const links = document.querySelectorAll(".nav-link");
+const title = document.querySelectorAll("h2");
+
+toggle.addEventListener("click", function (e) {
+  const nextIcon = e.target.getAttribute("data-set");
+  e.target.style.display = "none";
+  console.log(nextIcon);
+  //   document.querySelector("")
+  if (nextIcon === "sun") {
+    body.classList.add("dark-mode");
+    header.classList.add("header-dark-mode");
+    paragraph.classList.add("header-dark-mode");
+    links.forEach(function (e) {
+      e.classList.add("header-dark-mode");
+    });
+    document.querySelector(`.${nextIcon}`).style.display = "inline-block";
+    title.forEach(function (e) {
+      e.classList.add("dark-mode");
+    });
+  } else {
+    body.classList.remove("dark-mode");
+    header.classList.remove("header-dark-mode");
+    paragraph.classList.remove("header-dark-mode");
+    links.forEach(function (e) {
+      e.classList.remove("header-dark-mode");
+    });
+    document.querySelector(`.${nextIcon}`).style.display = "inline-block";
+    title.forEach(function (e) {
+      e.classList.remove("dark-mode");
+    });
+  }
+});
