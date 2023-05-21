219d7489a7ee0e059bb6f9b4a967980f9884f09b
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2023-05-21 21:41:03 +0530

    fixed navbar and added responsiveness

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ const paragraph = document.querySelector("p");
 const links = document.querySelectorAll(".nav-link");
 const title = document.querySelectorAll("h2");
 const para = document.querySelectorAll(".para");
+const ham = document.querySelector(".navbar-toggler");
+const menu = document.querySelector(".slidingMenu");
 
 if (localStorage.getItem("dark-mode") == "true") {
   body.classList.add("dark-mode");
@@ -41,6 +43,8 @@ if (localStorage.getItem("dark-mode") == "true") {
   });
   document.querySelector(`.sun`).style.display = "inline-block";
   document.querySelector(`.moon`).style.display = "none";
+  ham.style.filter = 'invert(0)';
+  menu.style.background = '#1c2020';
   title.forEach(function (e) {
     e.classList.add("dark-mode");
   });
@@ -53,6 +57,8 @@ toggle.addEventListener("click", function (e) {
   //   document.querySelector("")
   if (nextIcon === "sun") {
     body.classList.add("dark-mode");
+    menu.style.background = '#1c2020';
+    ham.style.filter = 'invert(0)';
     header.classList.add("header-dark-mode");
     paragraph.classList.add("header-dark-mode");
     links.forEach(function (e) {
@@ -65,6 +71,8 @@ toggle.addEventListener("click", function (e) {
     localStorage.setItem("dark-mode", true);
   } else {
     body.classList.remove("dark-mode");
+    menu.style.background = 'radial-gradient(#fff, rgb(255, 191, 191))';
+    ham.style.filter = 'invert(1)';
     header.classList.remove("header-dark-mode");
     paragraph.classList.remove("header-dark-mode");
     links.forEach(function (e) {
