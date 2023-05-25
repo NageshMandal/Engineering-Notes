76f273668eff0d75de8844924f6c7e1dbc4b298f
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2023-05-25 18:37:54 +0530

    fixed text under images

--- Gallary-section/theme.js
+++ Gallary-section/theme.js
@@ -2,12 +2,16 @@ const header = document.querySelector(".header");
 const toggle = document.querySelector(".toggle-mode");
 const body = document.querySelector("body");
 const links = document.querySelectorAll(".nav-link");
+const para = document.querySelectorAll("p");
 const h1 = document.querySelector("h1 a");
 if (localStorage.getItem("dark-mode") == "true") {
   body.classList.add("dark-mode");
   h1.classList.add("dark-mode");
   links.forEach(function (e) {
     e.classList.add("dark-mode");
+  });
+  para.forEach(function (e) {
+    e.style.color = "#2c3333";
   });
   document.querySelector(`.sun`).style.display = "inline-block";
   document.querySelector(`.moon`).style.display = "none";
@@ -24,6 +28,9 @@ toggle.addEventListener("click", function (e) {
     links.forEach(function (e) {
       e.classList.add("dark-mode");
     });
+    para.forEach(function (e) {
+      e.style.color = "#2c3333";
+    });
     document.querySelector(`.${nextIcon}`).style.display = "inline-block";
     localStorage.setItem("dark-mode", true);
   } else {
@@ -31,6 +38,9 @@ toggle.addEventListener("click", function (e) {
     h1.classList.remove("dark-mode");
     links.forEach(function (e) {
       e.classList.remove("dark-mode");
+    });
+    para.forEach(function (e) {
+      e.style.color = "white";
     });
     document.querySelector(`.${nextIcon}`).style.display = "inline-block";
     localStorage.setItem("dark-mode", false);
