e1613b4e26395901aea7f1469c6a22dcf3507858
PARAS <mehtaparas1901@gmail.com>
2023-06-15 19:44:44 +0530

    final commit

--- script.js
+++ script.js
@@ -144,6 +144,11 @@ function searchContributors(searchTerm) {
   }
 }
 
+// Sticky Navbar Function
+window.addEventListener("scroll", function() {
+  var navbar = document.querySelector('.navbar');
+  navbar.classList.toggle("sticky" , this.scrollY > 0);
+});
 
 
 
