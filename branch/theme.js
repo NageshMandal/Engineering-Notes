766bd3961f1c4e0693fb0348e1daaa4c2f37af71
PARAS <mehtaparas1901@gmail.com>
2023-06-15 19:26:44 +0530

    modfiy the navbar in all branch pages

--- branch/theme.js
+++ branch/theme.js
@@ -28,3 +28,9 @@ toggle.addEventListener("click", function (e) {
     });
   }
 });
+
+// Sticky Navbar Function
+window.addEventListener("scroll", function() {
+  var navbar = document.querySelector('.navbar');
+  navbar.classList.toggle("sticky" , this.scrollY > 0);
+});
